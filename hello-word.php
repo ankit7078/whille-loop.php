6d53ec0adb9921524d0a601776084e8e2cb15788
ankit7078 <av8537742@gmail.com>
2024-04-03 10:51:18 +0530

    add this file

---
+++ hello-word.php
@@ -0,0 +1,31 @@
+<?php
+$ankit="red";
+echo "<br>hello"."this is my new phone</br>";
+echo "hello-word", " ankit</br>";
+echo "<b><i><u><h1>ankit verma</h1></u></i></b>";
+echo 45.4;
+echo "<br>hello";
+print "ankit<br>";
+print("verma<br>");
+echo("$ankit<br>");
+print_r($ankit);
+?>
+<div>
+    this is my car
+    <p>that is my work</p>
+
+</div>
+<?php
+#$a=4;
+#$y="5";
+#echo "$a"+"$y";
+/*echo 4;
+echo "4.3";*/
+$x=5;
+function mytest(){
+    $x=56;
+    echo "variable is using this x $x<br>";
+}
+mytest();
+echo "variable is a using this text: $x";
+?>
---
+++ variable.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable</title>
+</head>
+<body>
+    <?php
+$x= "ankit bhai";
+echo $x." <br>";
+var_dump($x);
+    ?>
+
+    <br>
+<?php
+$y= 345;
+echo $y. "<br>";
+var_dump($y);
+?>
+
+    <br>
+<?php
+$y= "345";
+echo $y. "<br>";
+var_dump($y);
+?>
+    <br>
+<?php
+$y= 34.5;
+echo $y. "<br>";
+var_dump($y);
+?>
+
+    <br>
+<?php
+$y= true;
+echo $y. "<br>";
+var_dump($y);
+?>
+
+    <br>
+<?php
+$y= array("html","css","5");
+echo $y. "<br>";
+var_dump($y);
+?>
+</body>
+</html>
